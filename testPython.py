1e38d7a633e4b79bbc0f624da145dcaa8af10fe4
alket <alket.cecaj@unimore.it>
2021-06-01 10:48:14 +0200

    aggiunta azioni MSFT

--- testPython.py
+++ testPython.py
@@ -10,3 +10,13 @@ dati.to_csv("AzioniApple10Anni.csv")
 print("Applicazione che si connette a YF per scaricare i dati di Apple!")
 print(dati.head())
 
+
+t = yf.Ticker("MSFT")
+
+dati_msft = t.history(period = "10Y")
+
+dati_msft.to_csv("AzioniMSFT10Anni.csv")
+print("Applicazione che si connette a YF per scaricare i dati di Microsoft!")
+print(dati.head())
+
+
